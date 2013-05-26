8dfcbaf00d3951a22701165857a146414f6c412e
Green Media Agency <greenmedia@greenlaptop.(none)>
2013-05-26 06:37:45 -0600

    get sms p1

--- lib/sms_mod/receiver.rb
+++ lib/sms_mod/receiver.rb
@@ -3,8 +3,14 @@ module SmsMod
 
 		attr_accessor :message
 
-		def initialize(message)
-			@message = message
+		def initialize
+			@client = Twilio::REST::Client.new ENV['TWILIO_ASID'], ENV['TWILIO_ATOK']
+		end
+
+		def get_messages
+			@client.account.sms.messages.list({:date_sent => '2013-05-26'}).each do |sms|
+  				puts sms.id
+			end
 		end
 
 		def strip_message(options = [])
